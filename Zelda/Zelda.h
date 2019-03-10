ace1ba3ab87aadb7de9452bdfdb6dcc60b082305
TJC <chenyuha@oregonstate.edu>
2019-03-10 14:21:26 -0700

    fix bug of the wrong endings

--- Zelda/Zelda.h
+++ Zelda/Zelda.h
@@ -130,11 +130,14 @@ class Princess
 {
 private:
 	bool is_Alive;
+	bool along; //if princess is along with you
 
 public:
 	Princess();
 	~Princess();
 
+	inline bool	 getAlongState();
+	inline void	 setAlongState(bool);
 	inline bool	 getLivingState();
 	inline void	 setLivingState(bool);
 };
